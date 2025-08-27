cbdad07e85e345a87c4c2fce84735844164959b1
Nana Quaci <aducollins49@gmail.com>
2025-08-27 09:54:45 +0000

    Fixed Cart badge locator issues

--- src/main/java/com/projects/pages/CartPage.java
+++ src/main/java/com/projects/pages/CartPage.java
@@ -3,6 +3,12 @@ package com.projects.pages;
 import com.projects.base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
+
 
 public class CartPage extends BasePage {
 
@@ -10,8 +16,8 @@ public class CartPage extends BasePage {
     private final By removeFromCartButton = By.id("remove-sauce-labs-backpack");
     private final By cartIcon = By.className("shopping_cart_link");
     private final By cartItem = By.className("cart_item");
-//  private final By cartBadge = By.className("shopping_cart_badge");
-    private final By cartBadge = By.cssSelector("span[data-test='shopping-cart-badge']");
+    private final By cartBadge = By.className("shopping_cart_badge");
+//  private final By cartBadge = By.cssSelector("span[data-test='shopping-cart-badge']");
     private final By continueShoppingButton = By.id("continue-shopping");
 
     public CartPage(WebDriver driver) {
@@ -35,7 +41,9 @@ public class CartPage extends BasePage {
     }
 
     public String getCartBadgeCount() {
-        return driver.findElement(cartBadge).getText();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement badge = wait.until(ExpectedConditions.visibilityOfElementLocated(cartBadge));
+        return badge.getText();
     }
 
     public void clickContinueShopping() {
